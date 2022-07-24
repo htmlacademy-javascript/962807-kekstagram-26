3d42c319fe3456bfce925e2fbcd73dcfea02186e
Keks <keks@htmlacademy.ru>
2022-07-24 16:49:14 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import './full-size-picture-render.js';
+import './miniature-render.js';
+import './upload-form-view.js';
+import './full-size-picture-view.js';
+import './upload-form-validate.js';
+import './upload-form-zoom-photo.js';
+import './upload-form-processing-photo.js';
