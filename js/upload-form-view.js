1ab3e55169a1b31ceacbb66cb056817ac48a5cbc
Yury <marchenko.yr@yandex.ru>
2022-07-26 18:26:05 +0300

    Загрузка фото

--- js/upload-form-view.js
+++ js/upload-form-view.js
@@ -1,6 +1,7 @@
 import { isEscapeKey } from './util.js';
 import {addZoomHandler, removeZoomHandler, setZoomDefault} from './upload-form-zoom-photo.js';
 import {addEffectsHandler, removeEffectsHandler} from './upload-form-processing-photo.js';
+import { showAlertOnError } from './upload-form-alerts.js';
 
 const uploadPictureForm = document.querySelector('.img-upload__overlay');
 const uploadPictureButton = document.querySelector('#upload-file');
@@ -26,23 +27,24 @@ const resetPictureFormDefaults = () => {
   hashtag.value = '';
   textDescription.value = '';
   pictureInput.value = '';
+  picturePreview.src = '';
 };
 
 const uploadPicture = () => {
   const file = uploadPictureButton.files[0];
   const fileName = file.name.toLowerCase();
-
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
+  const matches = FILE_TYPES.some((type) => fileName.endsWith(type));
 
   if (matches) {
     picturePreview.src = URL.createObjectURL(file);
+    return true;
+  } else {
+    return showAlertOnError();
   }
-}
+};
 
 function onUploadPictureButtonClick() {
-  uploadPicture();
+  if (!uploadPicture()) {return;}
   uploadPictureForm.classList.remove('hidden');
   document.body.classList.add('modal-open');
   closePictureFormButton.addEventListener('click', onClosePictureFormButtonClick);
