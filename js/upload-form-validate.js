931d6942031b863e778ab5ceb8144396427d9e6a
Yury <marchenko.yr@yandex.ru>
2022-07-26 13:26:49 +0300

    Правки

--- js/upload-form-validate.js
+++ js/upload-form-validate.js
@@ -31,10 +31,7 @@ const checkHashtagsDuplicate = (value) => {
 
 const checkHashtagsLength = (value) => stringToWords(value).every((hashtag) => hashtag.length <= MAX_HASHTAG_LENGTH);
 
-const checkHashtagsChars = (value) => {
-  if (!value) {return true;}
-  return stringToWords(value).every((hashtag) => HASHTAG_REGULAR.test(hashtag));
-};
+const checkHashtagsChars = (value) => value ? stringToWords(value).every((hashtag) => HASHTAG_REGULAR.test(hashtag)) : true;
 
 // Добавляем настраиваемые валидаторы для хештегов
 pristine.addValidator(hashtagInput, checkHashtagsCount,
