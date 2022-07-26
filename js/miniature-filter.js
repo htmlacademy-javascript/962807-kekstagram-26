fdae532588f45cf04c5fe457e437046e59ad0e87
Yury <marchenko.yr@yandex.ru>
2022-07-26 17:16:52 +0300

    Фильтрация контента

--- js/miniature-filter.js
+++ js/miniature-filter.js
@@ -1,6 +1,6 @@
 import { contentData } from './network.js';
 import { renderContent, removeContent } from './miniature-render.js';
-import { shuffleArray } from './util.js';
+import { shuffleArray, debounce } from './util.js';
 
 const filterSection = document.querySelector('.img-filters');
 const filterForm = filterSection.querySelector('.img-filters__form');
@@ -8,36 +8,37 @@ const defaultButton = filterForm.querySelector('#filter-default');
 const randomButton = filterForm.querySelector('#filter-random');
 const popularButton = filterForm.querySelector('#filter-discussed');
 const DATA_MAX = 10;
+const RERENDER_DELAY = 500;
 
 const toggleCurrentFilter = (element) => {
   const activeSelector = 'img-filters__button--active';
-  const previousElement = filterForm.querySelector('.'+ activeSelector);
+  const previousElement = filterForm.querySelector(`.${ activeSelector}`);
   previousElement.classList.remove(activeSelector);
-  previousElement.disabled = false;
   element.classList.add(activeSelector);
-  if (element === randomButton) {return;}
-  element.disabled = true;
 };
+
+const updateContent = debounce((data) => {
+  removeContent();
+  renderContent(data);
+}, RERENDER_DELAY);
+
 
 const onRandomButtonClick = () => {
   toggleCurrentFilter(randomButton);
   const data = shuffleArray(contentData.slice()).slice(0, DATA_MAX);
-  removeContent();
-  renderContent(data);
+  updateContent(data);
 };
 
 const onPopularButtonClick = () => {
   toggleCurrentFilter(popularButton);
   const data = contentData.slice();
   data.sort((a, b) => b.comments.length - a.comments.length);
-  removeContent();
-  renderContent(data);
+  updateContent(data);
 };
 
 const onDefaultButtonClick = () => {
   toggleCurrentFilter(defaultButton);
-  removeContent();
-  renderContent(contentData);
+  updateContent(contentData);
 };
 
 defaultButton.addEventListener('click', onDefaultButtonClick);
