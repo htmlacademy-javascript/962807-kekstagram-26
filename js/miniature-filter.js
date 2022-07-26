d24a4a54fc633fd757ad68c3651d2cf15e89e092
Yury <marchenko.yr@yandex.ru>
2022-07-26 13:05:27 +0300

    Правки

--- js/miniature-filter.js
+++ js/miniature-filter.js
@@ -4,45 +4,64 @@ import { shuffleArray } from './util.js';
 
 const filterSection = document.querySelector('.img-filters');
 const filterForm = filterSection.querySelector('.img-filters__form');
-const defaultButton =filterForm.querySelector('#filter-default');
-const randomButton =filterForm.querySelector('#filter-random');
-const popularButton =filterForm.querySelector('#filter-discussed');
+const defaultButton = filterForm.querySelector('#filter-default');
+const randomButton = filterForm.querySelector('#filter-random');
+const popularButton = filterForm.querySelector('#filter-discussed');
 
 const DATA_MAX = 10;
 
 filterSection.classList.remove('img-filters--inactive');
+randomButton.classList.add('img-filters__button--active');
 
-const currentFilter = () => {
+const getActiveButton = () => {
 
-};
+}
 
-const debounce = () => {
-  filterForm.disable = true;
-  setTimeout(filterForm.disable = false, 5000);
-};
+const toggleCurrentFilter = (elem) => {
+  filterForm.querySelector('.img-filters__button--active').classList.remove()
+}
+
+// const currentFilter = () => {
+
+// };
+// const disable = () => {
+//   defaultButton.disabled = true;
+//   randomButton.disabled = true;
+//   popularButton.disabled = true;
+// };
+// const enable = () => {
+//   filterForm.disable = false;
+//   randomButton.disable = false;
+//   randomButton.disable = false;
+// };
+// const debounce = () => {
+//   disable();
+//   setTimeout(enable, 5000);
+// };
 
 
-const onRandomButtonClick = (event) => {
-  debounce();
+const onRandomButtonClick = () => {
   const data = shuffleArray(contentData.slice()).slice(0, DATA_MAX);
   removeContent();
   renderContent(data);
 };
 
-const onPopularButtonClick = (event) => {
-  debounce();
+const onPopularButtonClick = () => {
   const data = contentData.slice();
   data.sort((a, b) => b.comments.length - a.comments.length);
   removeContent();
   renderContent(data);
 };
 
-const onDefaultButtonClick = (event) => {
-  debounce();
+const onDefaultButtonClick = () => {
   removeContent();
   renderContent(contentData);
 };
 
-setTimeout(onRandomButtonClick, 5000);
-setTimeout(onPopularButtonClick, 9000);
-setTimeout(onDefaultButtonClick, 14000);
+defaultButton.addEventListener('click', onDefaultButtonClick);
+randomButton.addEventListener('click', onRandomButtonClick);
+popularButton.addEventListener('click', onPopularButtonClick);
+
+// setTimeout(onRandomButtonClick, 5000);
+// setTimeout(onRandomButtonClick, 3000);
+// setTimeout(onDefaultButtonClick, 14000);
